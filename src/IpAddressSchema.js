b43255e527564e035fac0eedc5341b53e47d9a5d
Yanoka2010 <yanasakrotenko1@gmail.com>
2024-11-12 16:56:50 +0300

    gg

---
+++ src/IpAddressSchema.js
@@ -0,0 +1,23 @@
+class IpValidator {
+  isRealIp(str) {
+    const digits = `${str}`.split('.');
+    return digits.at(0) === '27' && digits.length === 4;
+  }
+
+  ipAddressValues() {
+    this.valuesCheck = true;
+    return this;
+  }
+
+  isNormValues(str) {
+    if (!this.valuesCheck) return true;
+    const res = str.split('.').filter((digit) => digit >= 0 && digit < 256);
+    return res.length === 4;
+  }
+
+  isValid(str) {
+    return this.isRealIp(str);
+  }
+}
+
+export default IpValidator;
